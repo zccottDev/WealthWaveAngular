aa1ac997662ebd4cb70b908d8c7c65d1a425b2ba
RAHUL A P <rahul.ap@aot-technologies.com>
2025-04-22 21:33:49 -0700

    [feature] Add pages and basic config setup

---
+++ src/app/pages/transactions/transactions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transactions',
+  imports: [],
+  templateUrl: './transactions.component.html',
+  styleUrl: './transactions.component.scss'
+})
+export class TransactionsComponent {
+
+}
---
+++ src/polyfills.ts
@@ -0,0 +1,10 @@
+
+
+/** Evergreen browsers require these. **/
+import 'core-js/es/array';
+import 'core-js/es/object';
+import 'core-js/es/promise';
+
+// Import Zone.js required by Angular (unless using zoneless mode)
+import 'zone.js';  // Included with Angular CLI.
+import 'zone.js/testing' //testing
