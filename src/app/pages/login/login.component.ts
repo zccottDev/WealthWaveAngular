9f8bb2dc4a1f5daa273dc1a9b208468dd2016ad0
RAHUL A P <rahul.ap@aot-technologies.com>
2025-04-26 08:21:02 -0700

    started working on login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { PasswordInputComponent } from 'src/app/components/password-input/password-input.component';
+import { TextInputComponent } from 'src/app/components/text-input/text-input.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, TextInputComponent, PasswordInputComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginForm: FormGroup;
+
+  constructor(
+    private readonly fb: FormBuilder,
+  ) {
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: [''],
+    });
+  }
+
+  ngOnInit(): void {
+    // Initialize any data or perform actions when the component is loaded
+  }
+
+  onLogin(): void {
+    // Handle login logic here
+  }
+
+  onRegister(): void {
+    // Handle registration logic here
+  }
 }
